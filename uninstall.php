d8234e5034f726cda710a89e5200b1d0c17a79b9
KMax <maxim.kliakhin@gmail.com>
2026-06-25 19:24:41 +0300

    Improved uninstall process to enhance cleanup

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,12 @@
 <?php
 
-defined( 'ABSPATH' ) || exit;
+// Exit if accessed directly or not called by WordPress uninstall process.
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
 function prom_uninstall_cleanup() {
 	// Clear scheduled events
 	wp_clear_scheduled_hook( 'prom_update_stock_cron' );
+	wp_clear_scheduled_hook( 'prom_single_update_event' );
 
 	// Remove all plugin options
 	delete_option( 'prom_xml_url' );
@@ -18,9 +20,21 @@ function prom_uninstall_cleanup() {
 	delete_option( 'prom_xml_sku_prefix_3' );
 	delete_option( 'prom_xml_sku_prefix_4' );
 	delete_option( 'prom_xml_sku_prefix_5' );
+	delete_option( 'prom_xml_skip_price_1' );
+	delete_option( 'prom_xml_skip_price_2' );
+	delete_option( 'prom_xml_skip_price_3' );
+	delete_option( 'prom_xml_skip_price_4' );
+	delete_option( 'prom_xml_skip_price_5' );
 	delete_option( 'prom_xml_update_interval' );
+	delete_option( 'prom_xml_hide_variable_low_instock' );
+	delete_option( 'prom_xml_variable_low_instock_max' );
 	delete_option( 'telegram_user_ids' );
 	delete_option( 'telegram_token_id' );
+
+	// Remove leftover transients created during import sessions
+	delete_transient( 'prom_xml_import_variations_temp' );
+	delete_transient( 'prom_xml_selected_attributes_temp' );
+	delete_transient( 'prom_xml_importer_activated' );
 }
 
 prom_uninstall_cleanup();
